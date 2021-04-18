9ef64c9fa60bbec4c5ae1f532fb2ecefc138d987
bruderooo <szymonjacon@gmail.com>
2021-04-18 15:08:33 +0200

    Add countries dictionary

--- ksr/src/main/java/com/tul/ksr/zad1/StaticLists.java
+++ ksr/src/main/java/com/tul/ksr/zad1/StaticLists.java
@@ -1,12 +1,52 @@
 package com.tul.ksr.zad1;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class StaticLists {
     // Jak to zrobić żęby nie musieć sprawdzać i liczby pojedyńczej i mnogiej?
     // już wiem i jest to nawet zaimplementowane xd
-    static List<String> CURRENCY_LIST = Arrays.asList("dlr", "franc", "pound", "cts", "pct", "ecu", "bpd", "crown", "kwacha", "stg", "mark", "guilder");
+    static final List<String> CURRENCY_LIST = Arrays.asList("dlr", "franc", "pound", "cts", "pct", "ecu", "bpd", "crown", "kwacha", "stg", "mark", "guilder");
 
-    static List<String> COUNTRIES_LIST = Arrays.asList("afghanistan", "albania", "algeria", "american-samoa", "andorra", "angola", "anguilla", "antigua", "argentina", "aruba", "australia", "austria", "bahamas", "bahrain", "bangladesh", "barbados", "belgium", "belize", "benin", "bermuda", "bhutan", "bolivia", "botswana", "brazil", "british-virgin-islands", "brunei", "bulgaria", "burkina-faso", "burma", "burundi", "cameroon", "canada", "cape-verde", "cayman-islands", "central-african-republic", "chad", "chile", "china", "colombia", "congo", "costa-rica", "cuba", "cyprus", "czechoslovakia", "denmark", "djibouti", "dominica", "dominican-republic", "east-germany", "ecuador", "egypt", "el-salvador", "equatorial-guinea", "ethiopia", "fiji", "finland", "france", "french-guiana", "gabon", "gambia", "ghana", "gibraltar", "greece", "grenada", "guadeloupe", "guam", "guatemala", "guinea", "guinea-bissau", "guyana", "haiti", "honduras", "hong-kong", "hungary", "iceland", "india", "indonesia", "iran", "iraq", "ireland", "israel", "italy", "ivory-coast", "jamaica", "japan", "jordan", "kampuchea", "kenya", "kuwait", "laos", "lebanon", "lesotho", "liberia", "libya", "liechtenstein", "luxembourg", "macao", "madagascar", "malawi", "malaysia", "mali", "malta", "martinique", "mauritania", "mauritius", "mexico", "monaco", "morocco", "mozambique", "namibia", "nepal", "netherlands", "netherlands-antilles", "new-caledonia", "new-zealand", "nicaragua", "niger", "nigeria", "north-korea", "norway", "oman", "pakistan", "panama", "papua-new-guinea", "paraguay", "peru", "philippines", "poland", "portugal", "qatar", "romania", "rwanda", "saudi-arabia", "senegal", "seychelles", "sierra-leone", "singapore", "somalia", "south-africa", "south-korea", "spain", "sri-lanka", "sudan", "suriname", "swaziland", "sweden", "switzerland", "syria", "taiwan", "tanzania", "thailand", "togo", "tonga", "trinidad-tobago", "tunisia", "turkey", "uae", "uganda", "uk", "uruguay", "us-virgin-islands", "usa", "ussr", "vanuatu", "vatican", "venezuela", "vietnam", "west-germany", "western-samoa", "yemen-arab-republic", "yemen-demo-republic", "yugoslavia", "zaire", "zambia", "zimbabwe");
+    static final List<String> COUNTRIES_LIST = Arrays.asList("west-germany", "usa", "france", "uk", "canada", "japan");
+
+    static final Map<String, String> COUNTRIES_MAP;
+
+    static {
+        Map<String, String> map = new HashMap<>() {{
+            put("us", "usa");
+            put("usa", "usa");
+            put("america", "usa");
+            put("american", "usa");
+            put("americas", "usa");
+            put("americans", "usa");
+            put("washington", "usa");
+
+            put("west-germany", "west-germany");
+            put("germany", "west-germany");
+            put("german", "west-germany");
+            put("bonn", "west-germany");
+
+            put("france", "france");
+            put("frances", "france");
+            put("french", "france");
+            put("paris", "france");
+
+            put("uk", "uk");
+            put("british", "uk");
+            put("england", "uk");
+            put("englands", "uk");
+            put("london", "uk");
+
+            put("canada", "canada");
+            put("canadas", "canada");
+            put("canadian", "canada");
+            put("ottawa", "canada");
+
+            put("japan", "japan");
+            put("Japans", "japan");
+            put("japanese", "japan");
+            put("tokyo", "japan");
+        }};
+        COUNTRIES_MAP = Collections.unmodifiableMap(map);
+    }
 }
