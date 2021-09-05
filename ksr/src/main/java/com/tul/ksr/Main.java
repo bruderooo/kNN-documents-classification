e6324a77f9c8ba8b969dddce2abc06461b245e08
bruderooo <szymonjacon@gmail.com>
2021-09-05 10:12:47 +0200

    Final version, 05.09.2021

--- ksr/src/main/java/com/tul/ksr/Main.java
+++ ksr/src/main/java/com/tul/ksr/Main.java
@@ -5,7 +5,7 @@ import java.text.ParseException;
 
 import static com.tul.ksr.zad1.MenuZadFirst.run;
 
-public class App {
+public class Main {
     public static void main(String[] args) throws FileNotFoundException, ParseException {
         run();
     }
